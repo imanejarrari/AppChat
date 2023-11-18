21d5498f02e1fa3b55beee724659abf4c446874c
imanejarrari <benjarrariimane@gmail.com>
2023-11-18 02:51:33 +0100

    commit

--- src/clients/client.java
+++ src/clients/client.java
@@ -1,58 +1,74 @@
 package clients;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+
 public class client {
-	public static void main(String[] args) {
-		final Socket clientSocket;
-		final BufferedReader BR;
-		final PrintWriter PW;
-		final Scanner scanner=new Scanner(System.in);
-		int port=3000;
-		String host="127.0.0.1";
-		try {
-			clientSocket=new Socket(host,port);
-			PW=new PrintWriter(clientSocket.getOutputStream());
-			BR=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			 Thread sender = new Thread(new Runnable() {
-	                String msg;
-	                @Override
-	                public void run() {
-	                    while(true){
-	                        msg = scanner.nextLine();
-	                        PW.println(msg);
-	                        PW.flush();
-	                    }
-	                }
-	            });
-			 sender.start();
-	            Thread receiver = new Thread(new Runnable() {
-	                String msg;
-	                @Override
-	                public void run() {
-	                    try {
-	                        msg = BR.readLine();
-	                        while(msg!=null){
-	                            System.out.println("Server : "+msg);
-	                            msg = BR.readLine();
-	                        }
-	                        System.out.println("Server out of service");
-	                        PW.close();
-	                        clientSocket.close();
-	                        scanner.close();
-	                  
-	                    } catch (IOException e) {
-	                        e.printStackTrace();
-	                    }
-	                }
-	            });
-	            receiver .start();
-	    }catch (IOException e){
-	        e.printStackTrace();
-	        }
-		}
-	
+    public static void main(String[] args) {
+        // Déclarations des variables
+        final Socket clientSocket;
+        final BufferedReader BR;
+        final PrintWriter PW;
+        final Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Connexion au serveur sur le port 4000
+            clientSocket = new Socket("localhost", 4000);
+            int port = clientSocket.getLocalPort();
+            System.out.println(port);
+
+            // Initialisation des flux de communication avec le serveur
+            PW = new PrintWriter(clientSocket.getOutputStream());
+            BR = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // Thread pour envoyer des messages au serveur
+            Thread sender = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    while (true) {
+                        // Lire l'entrée utilisateur et envoyer au serveur
+                        msg = scanner.nextLine();
+                        PW.println(msg);
+                        PW.flush();
+                    }
+                }
+            });
+            sender.start();
+
+            // Thread pour recevoir des messages du serveur
+            Thread receiver = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    try {
+                        // Lire les messages du serveur et afficher sur le client
+                        msg = BR.readLine();
+                        while (msg != null) {
+                            System.out.println("Server : " + msg);
+                            msg = BR.readLine();
+                        }
+
+                        // Le serveur est hors service, fermeture des flux et du socket client
+                        System.out.println("Server out of service");
+                        PW.close();
+                        clientSocket.close();
+                        scanner.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            receiver.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/server/server.java
+++ src/server/server.java
@@ -1,4 +1,5 @@
 package server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,62 +7,73 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+
 public class server {
-  public static void main(String[] args) {
-	  final ServerSocket serverSocket ;
-      final Socket clientSocket ;
-      final BufferedReader BR;
-      final PrintWriter PW;
-      final Scanner scanner=new Scanner(System.in);
-      int port=3000;
-      try {
-		serverSocket=new ServerSocket(port);
-    	  clientSocket=serverSocket.accept();
-    	  PW=new PrintWriter(clientSocket.getOutputStream());
-    	  BR=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    	  Thread sender=new Thread(new Runnable() {
-    		  String msg;
-    		  @Override
-    		  public void run() {
-    			  while(true) {
-    				  msg=scanner.nextLine();
-    				  PW.println(msg);
-    				  PW.flush();
-    			  }
-    		  }
-    		  
-    	  });
-    	  sender.start();
+    public static void main(String[] args) {
+        // Déclarations des variables
+        final ServerSocket serverSocket;
+        final Socket clientSocket;
+        final BufferedReader BR;
+        final PrintWriter PW;
+        final Scanner scanner = new Scanner(System.in);
 
-          Thread receive= new Thread(new Runnable() {
-              String msg ;
-              @Override
-              public void run() {
-                  try {
-                      msg = BR.readLine();
-                     
-                      while(msg!=null){
-                          System.out.println("Client : "+msg);
-                          msg = BR.readLine();
-                      }
+        try {
+            // Création du ServerSocket avec un port fixe (4000)
+            serverSocket = new ServerSocket(4000);
+            int port = serverSocket.getLocalPort();
+            System.out.println(port);
 
-                      System.out.println("Client déconecté");
+            // Attente de la connexion du client
+            clientSocket = serverSocket.accept();
+            
+            // Initialisation des flux de communication avec le client
+            PW = new PrintWriter(clientSocket.getOutputStream());
+            BR = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                      PW.close();
-                      scanner.close();
-                      clientSocket.close();
-                      serverSocket.close();
-                  } catch (IOException e) {
-                      e.printStackTrace();
-                  }
-              }
-          });
-          receive.start();
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-    	  
-    			  
-      
-  }
+            // Thread pour envoyer des messages au client
+            Thread sender = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    while (true) {
+                        // Lire l'entrée utilisateur et envoyer au client
+                        msg = scanner.nextLine();
+                        PW.println(msg);
+                        PW.flush();
+                    }
+                }
+            });
+            sender.start();
+
+            // Thread pour recevoir des messages du client
+            Thread receive = new Thread(new Runnable() {
+                String msg;
+
+                @Override
+                public void run() {
+                    try {
+                        // Lire les messages du client et afficher sur le serveur
+                        msg = BR.readLine();
+                        while (msg != null) {
+                            System.out.println("Client : " + msg);
+                            msg = BR.readLine();
+                        }
+
+                        // Client déconnecté, fermeture des flux et sockets
+                        System.out.println("Client déconnecté");
+                        PW.close();
+                        scanner.close();
+                        clientSocket.close();
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            receive.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
